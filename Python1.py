c27aecb9c38e944d5f5babb49f058040b499b5de
Nanthawit <sadzasad747@gmail.com>
2024-08-11 15:26:01 +0700

    python2

---
+++ Python1.py
@@ -0,0 +1,90 @@
+"""
+#
+#Part: Python Comment
+#
+"""
+
+# This is a comment
+# v = s/t
+# v = ความเร็ว (m/s)
+# s = ระยะทาง (m)
+# t = เวลา (s)
+
+"""
+v= s/t
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t = เวลา (s)
+"""
+
+print("Hello World!!!")
+"""
+#
+# Part: Python Variables
+#
+"""
+x = 5   
+y = "Hey Brus"
+print(x, y)
+
+x = str(3)
+y = int(5)
+z = float1(7)
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: Variables Names
+#
+"""
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+MY_VAR = "John"
+my_var2 = "John"
+# 2my_var "John0"
+# my_var = "John"
+# my var = "John"
+
+# Camel Case
+myVariabaleName = "John"
+# Pascal Case
+myVariabaleName = "John"
+# Snake Case
+my_variabal_Name = "John"
+
+"""
+#
+# Part: Python String
+#
+"""
+x = "Hey Brus"
+print(x)
+
+y = """1 Hey Brus
+2 Hey Brus
+3 Hey Brus
+"""
+print(y)
+
+x = "Hey Brus"
+print(x[2])
+print(lan(x))
+print("Hey" in x)
+print("what'sup" not in x)
+print(x.upper())
+print(x,lower())
+
+print(x.replace("Brus", "sis"))
+print(x.split(""))
+
+a = "Apple"
+b = "Company"
+print(a + " " + b)
+
+
+price = 20
+word = F"Price: {price:.2f}"
+print(word)
---
+++ python2.py
@@ -0,0 +1,57 @@
+"""
+#
+# Part: Python Operators
+# Arithmtic Oparators
+#
+"""
+x = 4 + 5
+print(x)
+
+x = 5
+y = 6
+z = x + y + 1
+print("z = x + y + 1 --->",z)
+
+z = x / y
+v = x % y
+print(z, v)
+
+"""
+#
+# Part: Python Operators
+# Assignment Oparators
+#
+"""
+a = 1
+a += 5
+# a = a + 5
+print(a)
+
+"""
+#
+# Part: Python Operators
+# Comparison Oparators
+#
+"""
+a = 10
+b = 15
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+"""
+#
+# Part: Python Operators
+# Logical Oparators
+#
+"""
+a = 5
+b = 10
+print( (a < 5) and (b > 20) )
+print( (a == 5) or (a <= 10) )
+print( not (a == 5) or (a <= 4) )
+print( (a < 7) & (b > 7) )
+print( (a == 5) | ( b <= 10) )
